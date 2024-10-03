3a8cc68cce31b38f7026ba24db5bc61cf7f6b498
Yunus Emre Karakum <yunusemrekarakum@gmail.com>
2024-10-03 03:17:26 +0300

    Category and product added with factory. Meilisearch has been installed and tested.

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,16 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
 
 class Product extends Model implements HasMedia
 {
+    use HasFactory, InteractsWithMedia, Searchable;
     use HasFactory, InteractsWithMedia;
     protected $table = 'products';
     protected $guarded = [];
     public function category()
     {
         return $this->belongsTo(Category::class);
+    }
+    public function searchableAs() :string
+    {
+        return 'products_index';
+    }
+    public function toSearchableArray()
+    {
+        return [
+            'id' => (int) $this->id,
+            'title' => $this->title,
+            'image_path' => $this->image_path,
+            'price' => $this->price,
+            'features' => $this->features,
+            'colors' => $this->colors,
+        ];
     }
 }
