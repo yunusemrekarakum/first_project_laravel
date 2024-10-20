cb6aa3cc0b434837f647693d5bf127bf421e352a
Yunus Emre Karakum <yunusemrekarakum@gmail.com>
2024-10-20 13:41:18 +0300

    laravel echo pusher

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Events\ProductEvent;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
+
+Route::get('/event', function () {
+    dd(event(new ProductEvent('mesaj başarılı')));
+});
 
 Route::get('/{any}', function () {
     return view('app'); // app.blade.php dosyanız
